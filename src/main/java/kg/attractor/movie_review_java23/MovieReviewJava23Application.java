fa97947cb39212cbd169bc2a04d4217f9d9c0cb5
Aleksei Podstavin <gwinbleid86@gmail.com>
2025-03-14 19:15:52 +0600

    create project

---
+++ src/main/java/kg/attractor/movie_review_java23/MovieReviewJava23Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.movie_review_java23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewJava23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReviewJava23Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/movie_review_java23/MovieReviewJava23ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.movie_review_java23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewJava23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
