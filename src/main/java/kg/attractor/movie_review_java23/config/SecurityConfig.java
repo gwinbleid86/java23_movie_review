e5d214c9e39bbc55b9c8cb7a48197b45eb4a41e3
Aleksei Podstavin <gwinbleid86@gmail.com>
2025-03-28 21:59:42 +0600

    lesson 54

---
+++ src/main/java/kg/attractor/movie_review_java23/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package kg.attractor.movie_review_java23.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetails() {
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .authorities("FULL")
+//                .build();
+//
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("qwe"))
+//                .roles("GUEST")
+//                .authorities("READ_ONLY")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        String fetchUser = "select email, password, enabled " +
+                "from users " +
+                "where email = ?";
+        String fetchRoles = "select email, role " +
+                "from users u, role r " +
+                "where u.email = ? " +
+                "and u.role_id = r.id";
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(fetchUser)
+                .authoritiesByUsernameQuery(fetchRoles);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/reviews/**").fullyAuthenticated()
+                        .requestMatchers("/movies/**").hasAuthority("ADMIN")
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/kg/attractor/movie_review_java23/controller/ReviewController.java
@@ -0,0 +1,18 @@
+package kg.attractor.movie_review_java23.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("reviews")
+public class ReviewController {
+
+    @GetMapping("by_movie_id")
+    public void getByMovieId(@RequestParam(name = "movieId") Long movieId) {
+        System.out.println("getByMovieId: " + movieId);
+    }
+}
